4f5c801fedbdc89930400d304a77eedcfda35870
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2021-06-25 01:00:56 +0700

    Update and rename bulanpulsa_1.js to bulanpulsa_2.js

--- reds/bulanpulsa_2.js
+++ reds/bulanpulsa_2.js
@@ -1,5 +1,6 @@
 /* redS Start */
 location.hash.indexOf("#pay")>-1&&window.location.replace("/2020/12/payment-method.html"),
-location.hash.indexOf("#fb")>-1&&window.location.replace("https://facebook.com/hamdaniazzah"),
+location.hash.indexOf("#pulsa-reg")>-1&&window.location.replace("/2021/01/pulsa-reguler.html"),
+location.hash.indexOf("#token-pln")>-1&&window.location.replace("/2021/01/token-listrik-pln.html"),
 location.hash.indexOf("#ig")>-1&&window.location.replace("https://instagram.com/hamdaniazzah");
 /* redS End */
